e97546cbde97c702f0d5adac41994f387c82a2db
Guilherme_Bento <65615269+Poluxin21@users.noreply.github.com>
2024-10-30 16:39:05 -0300

    Correção de codigo duplicado, Adicionado Tracker de Rosto e Olhos

--- services/head_service.py
+++ services/head_service.py
@@ -1,12 +1,15 @@
-import mediapipe as mp
+import numpy as np
 
-def update_vjoy_hand_gestures(hand_landmarks, vjoy_device):
-    index_finger_tip = hand_landmarks.landmark[mp.solutions.hands.HandLandmark.INDEX_FINGER_TIP]
-    thumb_tip = hand_landmarks.landmark[mp.solutions.hands.HandLandmark.THUMB_TIP]
-    
-    distance = ((thumb_tip.x - index_finger_tip.x) ** 2 + (thumb_tip.y - index_finger_tip.y) ** 2) ** 0.5
-    
-    if distance < 0.1:
-        vjoy_device.set_button(1, 1)
-    else:
-        vjoy_device.set_button(1, 0)
+def calculate_head_movement(landmarks, frame_width, frame_height):
+    left_eye = np.array([(landmarks[362].x * frame_width, landmarks[362].y * frame_height)])
+    right_eye = np.array([(landmarks[33].x * frame_width, landmarks[33].y * frame_height)])
+
+    eye_center = (left_eye + right_eye) / 2
+
+    gaze_direction = "Center"
+    if eye_center[0, 0] < frame_width * 0.3:
+        gaze_direction = "Left"
+    elif eye_center[0, 0] > frame_width * 0.7:
+        gaze_direction = "Right"
+
+    return gaze_direction
