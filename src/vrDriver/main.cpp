02289305f04a8f6465313546ada62fa8b9ecca0e
Guilherme Bento da Silva <guilhermebentodasilva55@gmail.com>
2024-11-01 11:31:53 -0300

    Main.cpp configure

--- src/vrDriver/main.cpp
+++ src/vrDriver/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <openvr.h>
 
+// Inicialização do OpenVR
+bool initOpenVR()
+{
+    vr::EVRInitError error = vr::VRInitError_None;
+    vr::IVRSystem *vrSystem = vr::VR_Init(&error, vr::VRApplication_Scene);
+}
+
+
 int main() {
-    
+
 }
