fcb9f27c1783d7ac10c826cf35d85a530ffda6ec
Guilherme bento da Silva <guilhermebentodasilva55@gmail.com>
2024-11-02 12:48:36 -0300

    Removido integração de driver em c++ por hora e intrduzido com driver integrado usando blibioteca openvr do Python

--- main.py
+++ main.py
@@ -3,25 +3,24 @@ from infrastructure.HandDetectorAdapter import HandDetectorAdapter
 from infrastructure.SocketAdapter import SocketAdapter
 from application.HandTrackingService import HandTrackingService
 from interface.HandTrackingApp import HandTrackingApp
-from infrastructure.CompilerAndExecute import CppCompiler
+from infrastructure.VRDeviceController import VRDeviceController
 
 def main():
+    # Inicialização dos componentes
     camera = CameraCapture()
     detector = HandDetectorAdapter()
     socket_adapter = SocketAdapter()
+    vr_controller = VRDeviceController()
 
     tracking_service = HandTrackingService(detector, camera, socket_adapter)
     app = HandTrackingApp(tracking_service)
+    
+    vr_controller.start()
 
-    app.run()
+    try:
+        app.run()
+    finally:
+        vr_controller.stop()
 
 if __name__ == "__main__":
-    cpp_file = "src/vrDriver/main.cpp"
-    executable = "src/vrDriver/main"
-    include_dir = "src/vrDriver/openvr-2.5.1"
-    lib_name = "openvr_api"
-
-    compiler = CppCompiler(cpp_file, executable, include_dir, lib_name)
-    compiler.compile_and_run()
-    
     main()
